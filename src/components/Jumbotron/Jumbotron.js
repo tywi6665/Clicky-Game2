1d0eee130464d981a928b76c4384d3d8bdb1dcea
tywi6665 <tyletcole@gmail.com>
2019-02-21 12:59:40 -0700

    changed padding

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -15,7 +15,7 @@ class Jumbotron extends Component {
     render() {
         return (
             <div className="jumbotron">
-                <div className="container">
+                <div className="wrapper">
                     <h1 className="display-4">Clicky Game</h1>
                     <p className="lead">It's time to get your click on!! Your task is simple, click on the pictures below, but be sure not click on the same picture twice!</p>
                     <h2 className="score">Score: {this.props.score}</h2>
